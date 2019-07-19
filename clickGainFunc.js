ee6fec0609e9793c48f1d6d148712d4cdf9f3af8
bkoklu001 <52800665+bkoklu001@users.noreply.github.com>
2019-07-19 12:35:10 -0400

    v0.0.1

---
+++ clickGainFunc.js
@@ -0,0 +1,9 @@
+var cc = 0
+var ccClickValue = 1
+
+document.getElementById("ccCounter").innerHTML = "Computer Chips = " + 0
+
+function ccClickFunc(){
+	cc = cc + ccClickValue
+	document.getElementById("ccCounter").innerHTML = "Computer Chips = " + cc
+}
